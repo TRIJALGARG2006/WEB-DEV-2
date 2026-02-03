91d5a83281a3719c86562eaad9bbd6bade29ad2b
TRIJALGARG2006 <trijalgarg40@gmail.com>
2026-02-03 14:32:16 +0530

    learning

--- day 8/script.js
+++ day 8/script.js
@@ -10,3 +10,8 @@ for (let i = 0; i < 1; i++) {
     paragraph.style.lineHeight = "1.6";
     paragraph.style.fontFamily = "Arial, sans-serif";
 }
+
+
+const qs =document.querySelector("#content p")
+console.log(qs);
+qs.textContent="lorem djkabn. j d. eqjo. e j";
